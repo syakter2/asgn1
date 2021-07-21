b177effec59db647a33c4e39791e4d8c08838df0
syakter2 <87745683+syakter2@users.noreply.github.com>
2021-07-20 21:47:21 -0700

    Create App.java

---
+++ App.java
@@ -0,0 +1,52 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int n = 10;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < (i * 2) + 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < (2 * (n - i)) - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+
+    }
+}
